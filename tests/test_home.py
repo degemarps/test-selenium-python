559928ec08ff186705f3ade5678a49b85197f3e2
degemarps <degemarpereira@gmail.com>
2022-08-22 17:16:33 -0300

    testando

--- tests/test_home.py
+++ tests/test_home.py
@@ -3,9 +3,5 @@ from page_objects.home_page import HomePage
 
 class HomeTest(HomePage):
 
-    def setUp(self):
-        super().setUp()
-        self.open_page()
-
     def test_home_page(self):
         print()
