ed47ec02355c387e400e353ffbcaec9ce73028ca
degemarps <degemarpereira@gmail.com>
2022-08-23 13:52:59 -0300

    mudando os testes

--- tests/test_home.py
+++ tests/test_home.py
@@ -3,5 +3,9 @@ from page_objects.home_page import HomePage
 
 class HomeTest(HomePage):
 
+    def setUp(self):
+        super().setUp()
+        self.open_page()
+
     def test_home_page(self):
-        print()
+        self.find_element(HomePage.username)
