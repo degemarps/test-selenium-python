e6617d87b81a391e8c00cfa56b1d47d3cafd48b0
degemarps <degemarpereira@gmail.com>
2022-08-24 20:17:34 -0300

    voltando para os testes antigos

--- Jenkinsfile
+++ Jenkinsfile
@@ -12,7 +12,7 @@ pipeline {
         stage('run') {
             steps {
                 sh """
-                    docker run -d --name python_test_selenium python_tests
+                    docker run -d --name python_test_selenium -it python_tests
                 """
             }
         }
--- tests/test_home.py
+++ tests/test_home.py
@@ -1,19 +1,11 @@
-# from page_objects.home_page import HomePage
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
+from page_objects.home_page import HomePage
 
-def setUp():
-    # configura o caminho para o chromedriver
-    PATH = Service('/usr/local/lib/python3.9/site-packages/seleniumbase/drivers/chromedriver')
-    driver = webdriver.Chrome(service=PATH)
 
-    # acessa a página web
-    driver.get('https://www.saucedemo.com/')
+class HomeTest(HomePage):
 
-    return driver
+    def setUp(self):
+        super().setUp()
+        self.open_page()
 
-def test_home_page():
-    driver = setUp()
-    driver.get('https://www.saucedemo.com/')
-    driver.find_element(By.ID, 'user-name')
+    def test_home_page(self):
+        self.find_element(HomePage.username)
